30dcc99a1259891d4d7af12a4ce659f56feeb8d0
admin <admin@admins-MacBook-Pro.local>
2019-01-03 18:47:07 -0500

    fixed headers

--- app.js
+++ app.js
@@ -240,6 +240,12 @@ updateCachedTable().then((x) => {
 })
 var tableCacheTime = new Date();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/update/', function(req, res) {
   updateMeta();
   scrapeFollowers();
